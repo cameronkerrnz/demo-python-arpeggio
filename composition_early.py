e9749a73a79df73f9be072820f46dbc98fe9fc64
Cameron Kerr <cameron.kerr.nz@gmail.com>
2023-08-27 22:20:10 +1200

    improvements and bug fixes in "early" example

--- composition_early.py
+++ composition_early.py
@@ -1,16 +1,27 @@
 import arpeggio
 from arpeggio.peg import ParserPEG, PTNodeVisitor, visit_parse_tree
+import json
 
 class LabelVisitor(PTNodeVisitor):
 
     def visit_label(self, node, children):
-        return children[0]
+        # We either have a single child (a single composition, which is a dictionary)
+        # Or we have a list of parts (a list of dictionaries)
+        # We need to combine them into one dictionary
+        #
+        res = {}
+        for child in children:
+            res.update(child)
+        return res
     
     def visit_part(self, node, children) -> {str:[{str:float}]}:
         return {children[0]: children[1]}
 
     def visit_composition(self, node, children) -> [{str:float}]:
-        return children
+        res = {}
+        for child in children:
+            res.update(child)
+        return res
 
     def visit_part_name(self, node, children) -> str:
         return ' '.join(children)
@@ -28,6 +39,35 @@ class LabelVisitor(PTNodeVisitor):
         """Don't include SP (and SP+ etc.) in the AST
         This is useful to remove and sqeeze spaces"""
         return None
+
+def stringify(v: any) -> str:
+    """Reconstruct the label"""
+
+    # How can we tell if we're looking at a single composition, or
+    # a list of parts (with their composition)? We don't have any
+    # type distinction between parts and compositions; they are
+    # both dictionaries... so we need to to try and see how deep
+    # these dictionaries are.
+
+    # Structural pattern matching in Python doesn't seem to allow
+    # us to match any dictionary of dictionaries, so we'll just
+    # look at the 'first' (random) element of the dictionary; if
+    # it's value is a dict, then we know we're dealing with parts,
+    # rather than just simple compositions.
+    #
+    # This is probabaly the biggest indication that we need something
+    # that has carries abstract types in the AST, such as Named Tuples
+    # or Dataclasses
+
+    def pct(f) -> str:
+        s = f"{f*100:.1f}".replace(".0", "")
+        return f"{s}%"
+    
+    if isinstance(v[list(v)[0]], dict):
+        return "; ".join([f"{part}: {stringify(composition)}" for part, composition in v.items()])
+    else:
+        return ", ".join([f"{pct(proportion)} {material}" for material, proportion in v.items()])
+
 
 with open('label.peg') as g:
     label_grammar = g.read()
@@ -52,6 +92,8 @@ for input_ in [
     ast = label_parser.parse(input_)
     result = visit_parse_tree(ast, LabelVisitor())
 
-    print(result)
-
+    print("Input label : ", input_)
+    print("JSON        : ", json.dumps(result))
+    print("Cleaned     : ", stringify(result))
+    print("---")
 
